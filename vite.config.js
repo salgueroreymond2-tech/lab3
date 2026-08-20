375fbe66ae17662c07b868c215e49967bfae6343
isaaccascante1000-oss <andrescacascante1000@gmail.com>
2026-08-20 16:21:34 -0600

    pantalla inicial

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
+        home: resolve(import.meta.dirname, 'home.html'),
         dashboard: resolve(import.meta.dirname, 'src/page/index.html'),
         analista: resolve(import.meta.dirname, 'src/page/analista.html'),
         solicitante: resolve(import.meta.dirname, 'src/page/solicitante.html'),
